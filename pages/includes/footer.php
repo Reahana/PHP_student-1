a1b023ce7b916aba8c6b3f333fa1af6dabf779c5
Amanda Reahana Gomes <reahana233@gmail.com>
2021-12-16 23:28:54 +0600

    footer page

---
+++ pages/includes/footer.php
@@ -0,0 +1,17 @@
+
+<?php if (isset($_SESSION['id'])){?>
+<footer class=" bg-dark text-white"  >
+    <div class="container">
+         <div class="row">
+             <div class="col-md-12">
+                 <p class="text-center">Created by <span class="text-danger font-weight-bold">Amanda Reahana Gomes</span>  | &copy;  2021 All rights reserved.</p>
+             </div>
+         </div>
+    </div>
+
+</footer>
+<?php }?>
+<script src="../assets/js/jquery-3.6.0.js"></script>
+<script src="../assets/js/bootstrap.js"></script>
+</body>
+</html>

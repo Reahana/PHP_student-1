06d5b7f5a21dc5f241e4dd949d45eb586a1a2a14
Amanda Reahana Gomes <reahana233@gmail.com>
2021-12-17 11:56:27 +0600

    add 3 new fuctions

--- app/classes/Subject.php
+++ app/classes/Subject.php
@@ -13,7 +13,12 @@ class Subject extends  Database
 {
   private $subject;
   private $link;
-
+    private $sql;
+    private $queryResult;
+    private $data=[];
+    private $row;
+    private $i;
+    private $subjectInfo;
   public function __construct($data=null)
   {
       $this->link =$this->connection();
@@ -61,6 +66,55 @@ class Subject extends  Database
             {
                 die('Query problem'.mysqli_error($this->link));
             }
+        }
+    }
+    public function getAllSubjectById($id)
+    {
+
+        $this->sql ="SELECT * FROM subjects WHERE  id= '$id'";
+        $this->queryResult = mysqli_query($this->link ,$this->sql);
+        return mysqli_fetch_assoc($this->queryResult);
+    }
+    public  function  delete($id)
+    {
+
+        if ($this->link)
+        {
+            $this->subjectInfo= $this->getAllSubjectById($id);
+            $this->sql ="DELETE FROM subjects WHERE  id= '$id'";
+            if (mysqli_query($this->link ,$this->sql))
+            {
+                session_start();
+                $_SESSION['message']='Subject Deleted successfully';
+                header('Location: action.php?status=manageSubject');
+            }
+            else
+            {
+                die('Query problem'.mysqli_error($this->link));
+            }
+        }
+
+    }
+    public function updateSubject($subjectInfo)
+    {
+
+
+        if ($this->link)
+        {
+            $this->sql ="Update subjects SET name='$this->name' WHERE  id= '$subjectInfo[id]'";
+            if (mysqli_query($this->link ,$this->sql))
+            {
+                return 'User info update successfully';
+            }
+            else
+            {
+                die('Connection problem'.mysqli_error($this->link));
+            }
+
+        }
+        else
+        {
+            die('Connection problem');
         }
     }
 }
